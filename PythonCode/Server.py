ea9ea598aeb1f739c3584310ac95f4c0691c6a20
bsmeele <b.smeele@student.tudelft.nl>
2022-05-25 10:11:40 +0200

    added audio support and some small QoL changes

--- PythonCode/Server.py
+++ PythonCode/Server.py
@@ -1,5 +1,11 @@
 import asyncio
-#import serial_asyncio
+import serial
+try:
+    import serial_asyncio
+    operation_mode = None
+except ModuleNotFoundError:
+    print('Module serial_asyncio not found, continuing in test mode...')
+    operation_mode = False
 import random
 
 from GUI import GUI
@@ -9,12 +15,14 @@ from preprocessing import Preprocessing
 
 
 class Server:
-    def __init__(self):
+    def __init__(self, operation_mode):
         self.loop = True
         self.todo = set()
-        self.prop_id_translation = {'0x75': 'Temperature', '0xa7': 'Humidity'}
+        self.prop_id_translation = {'0x75': 'Temperature', '0xa7': 'Humidity', '0x79': 'Audio'}
 
-        #self.reader, self.writer = await serial_asyncio.open_serial_connection(url='/dev/ttyACM0', baudrate=115200)
+        self.reader = None
+        self.writer = None
+        self.operation_mode = operation_mode
 
         self.db = Database()
         self.publisher = Publisher(self.todo)
@@ -30,10 +38,13 @@ class Server:
         await asyncio.sleep(wait_time)
 
         # generate a random message within the format
-        msg = f'color clock {random.choice(["DATA", "REQ"])}: pubaddr receivedaddr {random.choice(["0x75", "0xa7"])} ' \
+        msg = f'color clock DATA: pubaddr receivedaddr {random.choice(["0x75", "0xa7", "0x79"])} ' \
               f'{random.randint(100, 3000)} END'
         print(msg)
         return msg
+
+    async def send_message(self):
+        self.writer.write(b'message send')
 
     # identify message type and pass to appropriate function
     async def message_handler(self, msg):
@@ -41,17 +52,11 @@ class Server:
 
         try:
             if parsed_msg[2] == 'DATA:':
-                data_type = self.prop_id_translation[str(parsed_msg[5])]
-                data = self.preprocessing.process_func[data_type](parsed_msg[6])
-                self.db.store(data_type, data)
-                self.publisher.publish(data_type)
-
-            elif parsed_msg[2] == 'REQ:':
-                # request type not implemented, currently same as data message
-                data_type = self.prop_id_translation[str(parsed_msg[5])]
-                data = self.preprocessing.process_func[data_type](parsed_msg[6])
-                self.db.store(data_type, data)
-                self.publisher.publish(data_type)
+                data_type = self.prop_id_translation.get(str(parsed_msg[5]))
+                data = self.preprocessing.process_func.get(data_type, lambda x: x)(parsed_msg[6])
+                if data is not None:
+                    self.db.store(data_type, data)
+                    self.publisher.publish(data_type)
 
             else:
                 print('message not supported')
@@ -60,14 +65,24 @@ class Server:
             print('message too short')
 
     async def run(self):
+        if self.operation_mode:
+            try:
+                self.reader, self.writer = await serial_asyncio.open_serial_connection(url='/dev/ttyACM0',
+                                                                                       baudrate=115200)
+            except serial.SerialException:
+                print('No USB connection found, continuing in test mode...')
+                self.operation_mode = False
+
         await self.gui.run()
         i = 0
 
         while self.loop:
             print(self.todo)
             # wait for a new message
-            #msg = await self.reader.readline()
-            msg = await self.get_message()
+            if self.operation_mode:
+                msg = await self.reader.readline()
+            else:
+                msg = await self.get_message()
 
             # add the message to a list of task to be completed
             self.todo.add(asyncio.create_task(self.message_handler(msg), name=f'{i}'))
@@ -93,5 +108,17 @@ class Server:
 
 
 if __name__ == '__main__':
-    server = Server()
+    while operation_mode is None:
+        print('Test or operation mode? t/o')
+        toinput = input()
+        if toinput == 't':
+            operation_mode = False
+            print('Continuing in test mode...')
+            break
+        elif toinput == 'o':
+            operation_mode = True
+            print('Continuing in operation mode...')
+            break
+        print('Unrecognized mode')
+    server = Server(operation_mode)
     asyncio.run(server.run())
--- PythonCode/preprocessing.py
+++ PythonCode/preprocessing.py
@@ -1,8 +1,19 @@
 
 class Preprocessing:
     def __init__(self):
-        self.process_func = {'Temperature': self.divide, 'Humidity': self.divide}
+        self.process_func = {'Temperature': self.divide, 'Humidity': self.divide, 'Audio': self.average_audio}
+        self.audio_total = 0
+        self.audio_samples = 0
 
     def divide(self, data):
         data = float(data)/100
         return data
+
+    def average_audio(self, data):
+        self.audio_total += int(data)
+        self.audio_samples += 1
+        if self.audio_samples >= 10:
+            audio_average = self.audio_total/self.audio_samples
+            self.audio_total = 0
+            self.audio_samples = 0
+            return audio_average
